7e18854a965d6b7d84cc7bbe8f9675bb11a6209a
MariadelaLuzGuGa <209452051@teschi.edu.mx>
2023-10-12 14:27:31 -0600

    loginyregistro

--- APIMARIA/api/models.py
+++ APIMARIA/api/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+# """
+# Codigo para La BASE DE DATOS de Servico Social del TESCHI
+# """
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# class Registros(models.Model):
+#     """Metodo de REGISTRO de la Primera tabla"""
+#     uname = models.CharField(max_length=30, primary_key=True)
+#     email = models.EmailField()
+#     pass1 = models.CharField(max_length=50)
+#     pass2 = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         texto = "{0}({1})"
+#         return texto.format(self.user, self.email)
--- APIMARIA/api/views.py
+++ APIMARIA/api/views.py
@@ -1,8 +1,52 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+"""
+Codigo en Vista para el Login
+"""
+from django.shortcuts import render, redirect
+from django.shortcuts import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
-class Home(APIView):
-    template_name='index.html'
-    def get(self, request):
-        return render(request,self.template_name)
+@login_required(login_url='login')
+def HomePage(request):
+    """a"""
+    return render (request, 'home.html')
+    
+def SignupPage(request):
+    """b"""
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+        
+        if pass1!=pass2:
+            messages.error(request, "Las contraseñas no coinciden vuelve a escribirlas")
+            # return HttpResponse("La Contraseña no coincide")
+        else:
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('login')
+        
+    return render (request,'registro.html')
+
+def LoginPage(request):
+    """c"""
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        # print(username,pass1)
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, 'El Usuario o Contraseña son incorrectos. Vuelva a ingresarlos')
+            # return HttpResponse ("El Usuario o Contraseña son Incorrectos")
+    return render (request, 'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
