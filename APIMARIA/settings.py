f4f4dcf87acaaa53ae128e49cbd53e77195e240b
MariadelaLuzGuGa <209452051@teschi.edu.mx>
2023-10-27 20:19:54 -0600

    otero

--- APIMARIA/settings.py
+++ APIMARIA/settings.py
@@ -80,14 +80,26 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'APIMARIA.wsgi.application'
-
-
+# $gunicorn APIMARIA.wsgi:application
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         # es el database
+#         'NAME': 'postgres',
+#         # el usuario
+#         'USER': 'postgres',
+#         'PASSWORD': 'AuJoMa.03',
+#         'HOST': 'localhost', 
+#         'PORT': '5432',     
         
+#     }
+# }
 
 DATABASES = {
     'default': {
