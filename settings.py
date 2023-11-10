879441b20640a311b38261e920bd1ed791e67d41
MariadelaLuzGuGa <209452051@teschi.edu.mx>
2023-11-09 20:17:53 -0600

    Nuevo

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-@^lpj**6#0)rbq_oamqmz=l&g8v4ny57$2ms&5zy59#7xxpl92
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['serviciosocial.onrender.com']
 
 # AUTH_USER_MODEL = 'api.Registros'
 
